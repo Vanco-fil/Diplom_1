fe065f879fb01fd0f6aa8121b73d3ccc7fc85848
Ivan Ashchepkov <zoxah-bob@mail.ru>
2024-06-22 21:36:49 +0300

    юнитики

---
+++ data.py
@@ -0,0 +1,14 @@
+class Data:
+    BLACK_BUN = "black bun"
+    WHITE_BUN = "white bun"
+    SESAME_BUN = "sesame bun"
+    PRICE_BLACK_BUN = 9
+    PRICE_WHITE_BUN = 10
+    PRICE_SESAME_BUN = 10.45
+    SPICY_SAUCE = "hot sauce"
+    PRICE_SPICY_SAUCE = 1.30
+    SWEET_SAUCE = "sweet sauce"
+    PRICE_SWEET_SAUCE = 2
+    COWBERRY_CREAM = "cowberry cream"
+    PRICE_COWBERRY_CREAM = 1.50
+    RECEIPT = '(==== black bun ====)\n= sauce sweet sauce =\n(==== black bun ====)\n\nPrice: 20'
---
+++ tests/test_burger.py
@@ -0,0 +1,61 @@
+import unittest
+from unittest.mock import Mock
+from praktikum.bun import Bun
+from praktikum.burger import Burger
+from praktikum.ingredient import Ingredient
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE
+from data import Data
+
+
+class TestBurger(unittest.TestCase):
+
+    def test_set_buns(self):
+        mock = Mock()
+        burger = Burger()
+        mock.get_name.return_value = Data.BLACK_BUN
+        mock.get_price.return_value = Data.PRICE_BLACK_BUN
+        burger.set_buns(mock)
+        self.assertEqual(burger.bun.get_name(), Data.BLACK_BUN) and self.assertEqual(burger.bun.get_price(), Data.PRICE_BLACK_BUN)
+
+    def test_add_ingredient(self):
+        mock = Mock()
+        burger = Burger()
+        mock.get_price.return_value = Data.PRICE_SPICY_SAUCE
+        mock.get_name.return_value = Data.SPICY_SAUCE
+        mock.get_type.return_value = INGREDIENT_TYPE_SAUCE
+        burger.add_ingredient(mock)
+        self.assertEqual(burger.ingredients, [mock])
+
+    def test_remove_ingredient(self):
+        burger = Burger()
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, Data.SPICY_SAUCE, Data.PRICE_SPICY_SAUCE)
+        burger.add_ingredient(ingredient)
+        burger.remove_ingredient(0)
+        self.assertEqual(burger.ingredients, [])
+
+    def test_move_ingredient(self):
+        burger = Burger()
+        ingredient_0 = Ingredient(INGREDIENT_TYPE_SAUCE, Data.SPICY_SAUCE, Data.PRICE_SPICY_SAUCE)
+        ingredient_1 = Ingredient(INGREDIENT_TYPE_SAUCE, Data.SWEET_SAUCE, Data.PRICE_SWEET_SAUCE)
+        burger.add_ingredient(ingredient_0)
+        burger.add_ingredient(ingredient_1)
+        burger.move_ingredient(0, 1)
+        self.assertEqual(burger.ingredients[1].name, Data.SPICY_SAUCE)
+
+    def test_get_price(self):
+        burger = Burger()
+        bun = Bun(Data.BLACK_BUN, Data.PRICE_BLACK_BUN)
+        burger.set_buns(bun)
+        sauce_1 = Ingredient(INGREDIENT_TYPE_SAUCE, Data.SPICY_SAUCE, Data.PRICE_SPICY_SAUCE)
+        burger.add_ingredient(sauce_1)
+        self.assertEqual(burger.get_price(), 19.3)
+
+    def test_get_receipt(self):
+        mock = Mock()
+        burger = Burger()
+        mock.get_name.return_value = Data.BLACK_BUN
+        mock.get_price.return_value = Data.PRICE_BLACK_BUN
+        burger.set_buns(mock)
+        sauce = Ingredient(INGREDIENT_TYPE_SAUCE, Data.SWEET_SAUCE, Data.PRICE_SWEET_SAUCE)
+        burger.add_ingredient(sauce)
+        self.assertEqual(burger.get_receipt(), Data.RECEIPT)
